67d8d3175dddcb539dcc737a79795ac6a07015a3
Alexis <aleperalta252@gmail.com>
2023-08-15 14:58:00 -0300

    Agregar archivos de proyecto.

---
+++ Practico1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Shared;
+using DataAccessLayer;
+using DataAccessLayer.DAL;
+using DataAccessLayer.IDALs;
+
+IDAL_Persona _personas = new DAL_Persona();
+
+do
+{
+    try
+    {
+        Console.WriteLine("Nueva Persona");
+        Persona p = new Persona();
+        Console.WriteLine("Ingrese el nombre de una persona");
+        p.Nombre = Console.ReadLine();
+        Console.WriteLine("Ingrese el documento de una persona");
+        p.Documento = Console.ReadLine();
+        _personas.Insert(p);
+        _personas.GetPersonas().ForEach(p => p.Print());
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine( e.ToString()); 
+    }
+
+} while (!Console.ReadLine().Equals("EXIT"));
+
---
+++ Shared/Persona.cs
@@ -0,0 +1,29 @@
+﻿namespace Shared
+{
+    public class Persona
+    {
+        public string Nombre { get; set; } = "-- Sin nombre --";
+
+        private string documento = "";
+        public string Documento
+        {
+            get { return documento; }
+            set
+            {
+                if (value.Length >= 7)
+                    documento = value;
+                else
+                    throw new Exception("El documento debe tener al menos 7 caracteres");
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Persona");
+            Console.WriteLine("     Documento:" + this.Documento);
+            Console.WriteLine("     Nombre:" + this.Nombre);
+
+        }
+
+    }
+}
